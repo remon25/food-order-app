a3d0c951ad6bb210a2407126416f94be73d0bec5
remon25 <70450564+remon25@users.noreply.github.com>
2024-11-28 19:16:37 +0200

    edit css

--- src/app/_components/menu/CartProduct.js
+++ src/app/_components/menu/CartProduct.js
@@ -16,12 +16,12 @@ export default function CartProduct({ product, onRemove, index }) {
       <div className="grow">
         <h3 className="font-bold text-[12px]">{product.name}</h3>
         {product.size && (
-          <div className="text-xs">
+          <div className="text-xs text-nowrap">
             Größe: <span>{product.size.name}</span>
           </div>
         )}
         {product.extras?.length > 0 && (
-          <div className="text-xs text-gray-500">
+          <div className="text-xs text-gray-500 text-nowrap">
             {product.extras.map((extra) => (
               <div key={extra.name}>
                 {extra.name} {extra.price} &euro;
