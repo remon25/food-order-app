e41b81b4235ec9e644ce817499c75b31fd0729fe
remon25 <70450564+remon25@users.noreply.github.com>
2024-12-05 03:32:05 +0200

    integerate ro expertorder api

--- src/app/api/delivery/route.js
+++ src/app/api/delivery/route.js
@@ -1,54 +1,223 @@
 // import mongoose from "mongoose";
 // import { Order } from "@/app/models/Order";
+// import { MenuItem } from "@/app/models/menuItems";
+// import { DeliveryPrice } from "@/app/models/DeliverPrices";
+// import validator from "validator";
+// import sanitizeHtml from "sanitize-html";
 
 // export async function POST(req) {
-//   mongoose.connect(process.env.MONGO_URL);
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
 
-//   const {
-//     cartProducts,
-//     address,
-//     subtotal,
-//     deliveryPrice,
-//     paymentMethod,
-//     orderType,
-//   } = await req.json();
+//     const {
+//       cartProducts,
+//       address,
+//       subtotal,
+//       deliveryPrice,
+//       paymentMethod = "cash",
+//       orderType,
+//     } = await req.json();
 
-//   let finalTotalPrice;
-//   let computedDeliveryPrice;
-//   if (orderType === "delivery") {
-//     finalTotalPrice = subtotal + deliveryPrice;
-//     computedDeliveryPrice = deliveryPrice;
-//   } else if (orderType === "pickup") {
-//     finalTotalPrice = subtotal;
-//     computedDeliveryPrice = 0;
+//     // Validate and sanitize address fields
+//     if (!address.name || !address.email || !address.phone) {
+//       return new Response("Missing required address fields", { status: 400 });
+//     }
+
+//     if (paymentMethod !== "cash") {
+//       return new Response("Error in payment method", { status: 400 });
+//     }
+
+//     // Validate email
+//     if (!validator.isEmail(address.email)) {
+//       return new Response("Invalid email format", { status: 400 });
+//     }
+
+//     // Sanitize address fields
+//     const sanitizedAddress = {
+//       ...address,
+//       name: sanitizeHtml(address.name),
+//       email: sanitizeHtml(address.email),
+//       phone: sanitizeHtml(address.phone),
+//       city: sanitizeHtml(address.city),
+//       streetAdress: sanitizeHtml(address.streetAdress),
+//       buildNumber: sanitizeHtml(address.buildNumber),
+//       postalCode: sanitizeHtml(address.postalCode),
+//       deliveryTime: sanitizeHtml(address.deliveryTime),
+//     };
+
+//     // Validate and sanitize orderType
+//     if (!["delivery", "pickup"].includes(orderType)) {
+//       return new Response("Invalid order type", { status: 400 });
+//     }
+
+//     // Validate paymentMethod
+//     if (!["paypal", "credit card", "cash"].includes(paymentMethod)) {
+//       return new Response("Invalid payment method", { status: 400 });
+//     }
+
+//     // Fetch delivery price and minimum order for the city
+//     let cityDeliveryInfo;
+//     if (orderType === "delivery") {
+//       cityDeliveryInfo = await DeliveryPrice.findOne({
+//         name: sanitizedAddress.city,
+//       }).lean();
+
+//       if (!cityDeliveryInfo) {
+//         return new Response("City not supported for delivery", { status: 400 });
+//       }
+
+//       if (cityDeliveryInfo.isFreeDelivery && deliveryPrice > 0) {
+//         return new Response("Delivery is free for this city", { status: 400 });
+//       }
+
+//       if (!cityDeliveryInfo.isFreeDelivery && deliveryPrice === 0) {
+//         return new Response("Delivery is not free for this city", {
+//           status: 400,
+//         });
+//       }
+
+//       if (subtotal < cityDeliveryInfo.minimumOrder) {
+//         return new Response(
+//           `The minimum order for delivery in ${sanitizedAddress.city} is ${cityDeliveryInfo.minimumOrder}`,
+//           { status: 400 }
+//         );
+//       }
+//     }
+
+//     // Calculate final total price and delivery price
+//     let finalTotalPrice;
+//     let computedDeliveryPrice;
+//     if (orderType === "delivery") {
+//       finalTotalPrice = subtotal + deliveryPrice;
+//       computedDeliveryPrice = deliveryPrice;
+//     } else {
+//       finalTotalPrice = subtotal;
+//       computedDeliveryPrice = 0;
+//     }
+
+//     // Fetch menu items for price validation
+//     const menuItems = await MenuItem.find({
+//       _id: { $in: cartProducts.map((item) => item._id) },
+//     }).lean();
+
+//     // Calculate subtotal dynamically and validate
+//     let calculatedSubtotal = 0;
+//     const sanitizedCartProducts = [];
+
+//     for (const product of cartProducts) {
+//       const menuItem = menuItems.find(
+//         (item) => item._id.toString() === product._id
+//       );
+
+//       if (!menuItem) {
+//         return new Response(
+//           `Product with _id ${product._id} does not exist in the database`,
+//           { status: 400 }
+//         );
+//       }
+
+//       if (menuItem.price !== parseFloat(product.price)) {
+//         return new Response(`Price mismatch for product ${product.name}`, {
+//           status: 400,
+//         });
+//       }
+
+//       let productTotal = menuItem.price;
+
+//       if (product.size) {
+//         const selectedSize = menuItem.sizes.find(
+//           (size) =>
+//             size._id.toString() === product.size._id &&
+//             size.price === product.size.price
+//         );
+
+//         if (!selectedSize) {
+//           return new Response(
+//             `Invalid size '${product.size.name}' for product ${product.name}`,
+//             { status: 400 }
+//           );
+//         }
+
+//         productTotal += selectedSize.price;
+//       }
+
+//       if (product.extras && product.extras.length > 0) {
+//         for (const extra of product.extras) {
+//           const selectedExtra = menuItem.extraIngredientPrice.find(
+//             (menuExtra) =>
+//               menuExtra._id.toString() === extra._id &&
+//               menuExtra.price === extra.price
+//           );
+
+//           if (!selectedExtra) {
+//             return new Response(
+//               `Invalid extra '${extra.name}' for product ${product.name}`,
+//               { status: 400 }
+//             );
+//           }
+
+//           productTotal += selectedExtra.price;
+//         }
+//       }
+
+//       calculatedSubtotal += productTotal;
+
+//       const sanitizedProduct = {
+//         _id: product._id,
+//         image: sanitizeHtml(product.image),
+//         name: sanitizeHtml(product.name),
+//         description: sanitizeHtml(product.description),
+//         category: product.category,
+//         price: parseFloat(product.price),
+//         sizes: product.sizes,
+//         extraIngredientPrice: product.extraIngredientPrice,
+//         size: product.size,
+//         extras: product.extras,
+//       };
+
+//       sanitizedCartProducts.push(sanitizedProduct);
+//     }
+
+//     if (calculatedSubtotal !== subtotal) {
+//       return new Response(
+//         `Subtotal mismatch. Calculated: ${calculatedSubtotal}, Provided: ${subtotal}`,
+//         { status: 400 }
+//       );
+//     }
+
+//     const orderDoc = await Order.create({
+//       name: sanitizedAddress.name,
+//       email: sanitizedAddress.email,
+//       phone: sanitizedAddress.phone,
+//       city: sanitizedAddress.city,
+//       streetAdress: sanitizedAddress.streetAdress,
+//       buildNumber: sanitizedAddress.buildNumber,
+//       postalCode: sanitizedAddress.postalCode,
+//       deliveryTime: sanitizedAddress.deliveryTime,
+//       cartProducts: sanitizedCartProducts,
+//       paid: false,
+//       payOnDelivery: true,
+//       subtotal,
+//       deliveryPrice: computedDeliveryPrice,
+//       finalTotalPrice,
+//       paymentMethod,
+//       orderType,
+//     });
+
+//     const success_url =
+//       process.env.NEXTAUTH_URL +
+//       "orders/" +
+//       orderDoc._id.toString() +
+//       "?clear-cart=1";
+
+//     return Response.json(success_url);
+//   } catch (error) {
+//     console.error(error);
+//     return new Response("Internal Server Error", { status: 500 });
 //   }
-
-//   const orderDoc = await Order.create({
-//     name: address.name,
-//     email: address.email,
-//     phone: address.phone,
-//     city: address.city,
-//     streetAdress: address.streetAdress,
-//     buildNumber: address.buildNumber,
-//     postalCode: address.postalCode,
-//     deliveryTime: address.deliveryTime,
-//     cartProducts,
-//     paid: false,
-//     payOnDelivery: true,
-//     subtotal,
-//     deliveryPrice: computedDeliveryPrice,
-//     finalTotalPrice,
-//     paymentMethod: "cash",
-//     orderType,
-//   });
-
-//   const success_url =
-//     process.env.NEXTAUTH_URL +
-//     "orders/" +
-//     orderDoc._id.toString() +
-//     "?clear-cart=1";
-
-//   return Response.json(success_url);
 // }
 
 import mongoose from "mongoose";
@@ -74,24 +243,15 @@ export async function POST(req) {
       orderType,
     } = await req.json();
 
-
-
     // Validate and sanitize address fields
     if (!address.name || !address.email || !address.phone) {
       return new Response("Missing required address fields", { status: 400 });
     }
 
-    if (paymentMethod !== "cash") {
-      return new Response("Error in payment method", { status: 400 });
-    }
-    
-
-    // Validate email
     if (!validator.isEmail(address.email)) {
       return new Response("Invalid email format", { status: 400 });
     }
 
-    // Sanitize address fields
     const sanitizedAddress = {
       ...address,
       name: sanitizeHtml(address.name),
@@ -104,17 +264,14 @@ export async function POST(req) {
       deliveryTime: sanitizeHtml(address.deliveryTime),
     };
 
-    // Validate and sanitize orderType
     if (!["delivery", "pickup"].includes(orderType)) {
       return new Response("Invalid order type", { status: 400 });
     }
 
-    // Validate paymentMethod
     if (!["paypal", "credit card", "cash"].includes(paymentMethod)) {
       return new Response("Invalid payment method", { status: 400 });
     }
 
-    // Fetch delivery price and minimum order for the city
     let cityDeliveryInfo;
     if (orderType === "delivery") {
       cityDeliveryInfo = await DeliveryPrice.findOne({
@@ -125,16 +282,6 @@ export async function POST(req) {
         return new Response("City not supported for delivery", { status: 400 });
       }
 
-      if (cityDeliveryInfo.isFreeDelivery && deliveryPrice > 0) {
-        return new Response("Delivery is free for this city", { status: 400 });
-      }
-
-      if (!cityDeliveryInfo.isFreeDelivery && deliveryPrice === 0) {
-        return new Response("Delivery is not free for this city", {
-          status: 400,
-        });
-      }
-
       if (subtotal < cityDeliveryInfo.minimumOrder) {
         return new Response(
           `The minimum order for delivery in ${sanitizedAddress.city} is ${cityDeliveryInfo.minimumOrder}`,
@@ -143,7 +290,6 @@ export async function POST(req) {
       }
     }
 
-    // Calculate final total price and delivery price
     let finalTotalPrice;
     let computedDeliveryPrice;
     if (orderType === "delivery") {
@@ -154,12 +300,10 @@ export async function POST(req) {
       computedDeliveryPrice = 0;
     }
 
-    // Fetch menu items for price validation
     const menuItems = await MenuItem.find({
       _id: { $in: cartProducts.map((item) => item._id) },
     }).lean();
 
-    // Calculate subtotal dynamically and validate
     let calculatedSubtotal = 0;
     const sanitizedCartProducts = [];
 
@@ -221,7 +365,7 @@ export async function POST(req) {
 
       calculatedSubtotal += productTotal;
 
-      const sanitizedProduct = {
+      sanitizedCartProducts.push({
         _id: product._id,
         image: sanitizeHtml(product.image),
         name: sanitizeHtml(product.name),
@@ -232,9 +376,7 @@ export async function POST(req) {
         extraIngredientPrice: product.extraIngredientPrice,
         size: product.size,
         extras: product.extras,
-      };
-
-      sanitizedCartProducts.push(sanitizedProduct);
+      });
     }
 
     if (calculatedSubtotal !== subtotal) {
@@ -262,6 +404,85 @@ export async function POST(req) {
       paymentMethod,
       orderType,
     });
+
+    const expertOrderData = {
+      version: 1,
+      broker: "antalya-harsefeld.de",
+      id: orderDoc._id.toString(),
+      ordertime: new Date().toISOString(),
+      deliverytime: new Date().toISOString(),
+      customerinfo: sanitizedAddress.name,
+      orderprice: finalTotalPrice,
+      orderdiscount: 0,
+      notification: orderType === "delivery" ? false : true,
+      deliverycost: computedDeliveryPrice,
+      customer: {
+        phone: sanitizedAddress.phone,
+        email: sanitizedAddress.email,
+        name: sanitizedAddress.name,
+        street:
+          orderType === "delivery"
+            ? `${sanitizedAddress.streetAdress} ${sanitizedAddress.buildNumber}`
+            : "Ing.-Honnef-Str. 13",
+        zip: orderType === "delivery" ? sanitizedAddress.postalCode : "21509",
+        location: orderType === "delivery" ? sanitizedAddress.city : "Glinde",
+      },
+      payment: {
+        type: 0,
+        provider: "",
+        transactionid: "",
+        prepaid: 0,
+      },
+      items: sanitizedCartProducts.map((product) => {
+        const items = [];
+
+        if (product.size) {
+          items.push({
+            name: product.size.name,
+            price: product.size.price,
+            count: 1,
+          });
+        }
+
+        if (product.extras && product.extras.length > 0) {
+          product.extras.forEach((extra) => {
+            items.push({
+              name: extra.name,
+              price: extra.price,
+              count: 1,
+            });
+          });
+        }
+
+        return {
+          count: 1,
+          name: product.name,
+          price: product.price,
+          items, // Ensure each product has its own `items` array
+          type: "FOOD",
+        };
+      }),
+    };
+
+
+    // Send the order data to ExpertOrder API using PUT
+    const response = await fetch(process.env.EXPERTORDER_URL, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        API_KEY: process.env.EXPERTORDER_API_KEY,
+      },
+      body: JSON.stringify(expertOrderData),
+    });
+
+    if (!response.ok) {
+      console.error("ExpertOrder API Error:", await response.text());
+      return new Response("Failed to push order to ExpertOrder", {
+        status: 500,
+      });
+    } else {
+      console.log("Order pushed to ExpertOrder successfully");
+    }
 
     const success_url =
       process.env.NEXTAUTH_URL +
--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -4,19 +4,120 @@ import { Order } from "@/app/models/Order";
 export async function POST(req) {
   const sig = req.headers.get("Stripe-Signature");
   let event;
+
+  // Verify the Stripe event signature
   try {
     const reqBuffer = await req.text();
     const signSecret = process.env.STRIPE_SIGN_SECRET;
     event = stripe.webhooks.constructEvent(reqBuffer, sig, signSecret);
   } catch (err) {
     console.log(`❌ Error message: ${err.message}`);
+    return new Response("Webhook signature verification failed", {
+      status: 400,
+    });
   }
+
+  // Handle the successful payment event
   if (event.type === "checkout.session.completed") {
     const orderId = event?.data?.object?.metadata?.orderId;
     const isPaid = event?.data?.object?.payment_status === "paid";
+
     if (isPaid) {
+      // Update the order status to paid
       await Order.updateOne({ _id: orderId }, { paid: true });
+
+      // Fetch the order from your database (assuming Order.findById or similar method)
+      const orderDoc = await Order.findById(orderId);
+
+      // Prepare the data for ExpertOrder
+      const expertOrderData = {
+        version: 1,
+        broker: "antalya-harsefeld.de",
+        id: orderDoc._id.toString(),
+        ordertime: new Date().toISOString(),
+        deliverytime: new Date().toISOString(),
+        customerinfo: orderDoc.name,
+        orderprice: orderDoc.finalTotalPrice,
+        orderdiscount: 0,
+        notification: orderDoc.orderType === "delivery" ? false : true,
+        deliverycost: orderDoc.deliveryPrice,
+        customer: {
+          phone: orderDoc.phone,
+          email: orderDoc.email,
+          name: orderDoc.name,
+          street:
+            orderDoc.orderType === "delivery"
+              ? `${orderDoc.streetAdress} ${orderDoc.buildNumber}`
+              : "Ing.-Honnef-Str. 13",
+          zip:
+            orderDoc.orderType === "delivery" ? orderDoc.postalCode : "21509",
+          location:
+            orderDoc.orderType === "delivery" ? orderDoc.city : "Glinde",
+        },
+        payment: {
+          type: 3,
+          provider: "Stripe",
+          transactionid: event?.data?.object?.payment_intent,
+          prepaid: orderDoc.finalTotalPrice,
+        },
+        items: orderDoc.cartProducts.map((product) => {
+          const items = [];
+
+          if (product.size) {
+            items.push({
+              name: product.size.name,
+              price: product.size.price,
+              count: 1,
+            });
+          }
+
+          if (product.extras && product.extras.length > 0) {
+            product.extras.forEach((extra) => {
+              items.push({
+                name: extra.name,
+                price: extra.price,
+                count: 1,
+              });
+            });
+          }
+
+          return {
+            count: 1,
+            name: product.name,
+            price: product.price,
+            items, // Ensure each product has its own `items` array
+            type: "FOOD",
+          };
+        }),
+      };
+
+      // Send the order data to ExpertOrder API using PUT
+      try {
+        const response = await fetch(process.env.EXPERTORDER_URL, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            API_KEY: process.env.EXPERTORDER_API_KEY,
+          },
+          body: JSON.stringify(expertOrderData),
+        });
+
+        if (!response.ok) {
+          console.error("ExpertOrder API Error:", await response.text());
+          return new Response("Failed to push order to ExpertOrder", {
+            status: 500,
+          });
+        } else {
+          console.log("Order pushed to ExpertOrder successfully");
+        }
+      } catch (err) {
+        console.error("Error while sending data to ExpertOrder:", err);
+        return new Response("Failed to push order to ExpertOrder", {
+          status: 500,
+        });
+      }
     }
   }
+
   return Response.json({ received: true });
 }
