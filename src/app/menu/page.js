592d89cd1f6229a4ca0a40f11eaed8180630013a
remon25 <70450564+remon25@users.noreply.github.com>
2024-12-10 15:12:14 +0200

    edit menu

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,70 +1,32 @@
-import Sidebar from "../_components/layout/Sidebar";
-import MenuItem from "../_components/menu/MenuItem";
+import { redirect } from "next/navigation";
 
-export default async function page() {
-  // Fetch data directly in the Server Component
-  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/menu-items`, {
-    cache: "no-store",
-  });
-  const menu = await response.json();
-  // Fetch data directly in the Server Component
-  const Categoryresponse = await fetch(
-    `${process.env.NEXTAUTH_URL}/api/categories`,
-    {
-      cache: "no-store",
-    }
-  );
-  const categories = await Categoryresponse.json();
+export const metadata = {
+  title: "OUR MENU",
+  description:
+    "Antalya in Harsefeld bietet frisches und leckeres Sushi mit schneller Lieferung. Genießen Sie hochwertige Sushi-Kreationen, bequem und frisch direkt nach Hause geliefert. Jetzt bestellen!",
+  icons: {
+    icon: "/logo-favi.png",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://antalya-harsefeld.de/menu/",
+    title: "OUR MENU - Antalya Harsefeld",
+    description:
+      "Antalya in Harsefeld bietet frisches und leckeres Sushi mit schneller Lieferung. Genießen Sie hochwertige Sushi-Kreationen, bequem und frisch direkt nach Hause geliefert. Jetzt bestellen!",
+    images: [
+      {
+        url: "/logo-og.png",
+        width: 1200,
+        height: 630,
+        alt: "Antalya Harsefeld",
+      },
+    ],
+  },
+};
 
-  // Reorder categories to make "offers" the first category
-  const reorderedCategories = categories.sort((a, b) => {
-    if (a.name.toLowerCase() === "offers") return -1;
-    if (b.name.toLowerCase() === "offers") return 1;
-    return 0; // Keep the other categories in the same order
-  });
+export default function Page() {
+  // Redirect immediately when the component loads
+  redirect("/");
 
-  return (
-    <section className="home-menu pr-[330px] mr-4">
-      <Sidebar />
-      <h1 className="text-[#222] text-center font-bold text-2xl md:text-3xl mb-10 p-2">
-        🍕  Antalya Speisekarte 🍕
-      </h1>
-      {reorderedCategories.length > 0 &&
-        reorderedCategories.map((c) => {
-          const isOffersCategory = c.name.toLowerCase() === "offers";
-
-          return (
-            <div
-              key={c._id}
-              className={isOffersCategory ? "offers-category" : ""}
-            >
-              <div
-                className={`pl-7 ${
-                  isOffersCategory ? "custom-offers-header" : ""
-                }`}
-              >
-                <h2 className="text-gray-800 font-bold text-[1rem] md:text-2xl mt-14 mb-4">
-                  {c.name}
-                </h2>
-              </div>
-              <div
-                className={`grid md:grid-cols-1 md:justify-center gap-6 mt-6 mb-12 px-5 ${
-                  isOffersCategory ? "custom-offers-grid" : ""
-                }`}
-              >
-                {menu
-                  .filter((item) => item.category === c._id)
-                  .map((item) => (
-                    <MenuItem
-                      key={item._id}
-                      menuItemInfo={item}
-                      isOffersCategory={isOffersCategory}
-                    />
-                  ))}
-              </div>
-            </div>
-          );
-        })}
-    </section>
-  );
+  return null; // No need to render anything, just redirecting
 }
